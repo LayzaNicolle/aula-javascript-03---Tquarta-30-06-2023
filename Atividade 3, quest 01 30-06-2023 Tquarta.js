2711bc62feab51e6000233b8a989264785c8a1b1
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-30 16:37:24 -0300

    Ativ 03, quest 02 - aula 03 30-06-2023 Tquarta

---
+++ Atividade 3, quest 01 30-06-2023 Tquarta.js
@@ -0,0 +1,13 @@
+
+//Atividade 3, quest 01
+
+var x = 5;
+var y = 7;
+
+if (x < y) {
+  console.log("Olá mundo!");
+} else if (x > y) {
+  console.log("Mundo, olá!");
+} else {
+  console.log("Adeus!");
+}
