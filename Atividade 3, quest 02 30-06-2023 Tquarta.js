38a5ea19451a4a251c45fdb1d478fd721cde9ed3
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-30 16:41:29 -0300

    Ativ 03 , quest 02 - Aula 03 30-06-2023 Tquarta

---
+++ Atividade 3, quest 02 30-06-2023 Tquarta.js
@@ -0,0 +1,14 @@
+
+///Atividade 03, quest 02
+
+var fruta = prompt("Qual é a fruta?");
+
+if (fruta === "banana") {
+  console.log("O filme é: Os minions.");
+} else if (fruta === "laranja") {
+  console.log("O filme é: Laranja Mecânica.");
+} else if (fruta === "maçã") {
+  console.log("O filme é: Branca de neve.");
+} else {
+  console.log("O cinema tá fechado!");
+}
