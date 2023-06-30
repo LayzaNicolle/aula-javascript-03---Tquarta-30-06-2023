06a0925f686bdc2c105f3b52d53ed0c64e6bc653
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-30 15:32:34 -0300

    Ativ 1, quest 01 30-06-2023 Tquarta (corrigida)

---
+++ Atividade 1, quest 01 30-06-2023 Tquarta (corrigida).js
@@ -0,0 +1,11 @@
+//Questão 01
+
+var idade = prompt("Qual é a sua idade?")
+
+if(idade>=18){
+  console.log("Que legal! Você já pode ter a carteira de habilitação!")
+} else {
+  console.log("Poxa, que pena! Você ainda não pode ter a carteira de habilitação!")
+}
+
+
