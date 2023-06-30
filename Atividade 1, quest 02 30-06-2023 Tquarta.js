07c320ba76bee9ea9700aa27494075c381316036
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-30 15:36:46 -0300

    Ativ 1, questão 02 (Aula 03): 30-06-2023 Tquarta

---
+++ Atividade 1, quest 02 30-06-2023 Tquarta.js
@@ -0,0 +1,10 @@
+
+
+//Questão 02
+
+var pergunta = prompt ("Você terminou o ensino médio? [sim] ou [nao] ")
+if(pergunta=="sim"){
+  console.log("Poxa, que legal!")
+} else {
+  console.log("Falta pouco! Logo você termina.")
+}
